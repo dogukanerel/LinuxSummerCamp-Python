6636c41f84fd749b676b51dad7b6a919ca874a8a
Doğukan Erel <dogukanerel97@gmail.com>
2017-08-15 22:12:39 +0300

    finding prime numbers with multiprocessing

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -34,6 +34,6 @@ if __name__ == "__main__":
     print(time.time() - eski)
     print(sonuclar)
     alt_toplamlar = map(sum, sonuclar)
-    print("Sayilarin Toplami:", sum(alt_toplamlar))
+    print("Sayilarin Toplami:", sum(alt_toplamlar)) 
 
 
